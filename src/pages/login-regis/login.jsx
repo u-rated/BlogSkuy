117289b59121c0d114460c8464eea770d131da84
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2023-04-04 23:13:52 +0700

    Added login-registration folder in src -> pages
    - Added login.jsx (Header, illust img, labels & inputs)

---
+++ src/pages/login-regis/login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function login() {
+  return (
+    <>
+    <h1>Blogskuy Login</h1>
+    <div>
+        <p>Blog</p>
+        <p>Skuy</p>
+    </div>
+    <form action="">
+        <div>
+            <label htmlFor="logEmail">Email</label>
+            <input type="text" name='logEmail' id='logEmail' />
+        </div>
+        <div>
+            <label htmlFor="password">Password</label>
+            <input type='password' name='password' id='password' />
+        </div>
+    </form>
+    </>
+  )
+}
